7a421a973bf8297e0958072c3745479d14dfc339
MinhTinh <20130438@st.hcmuaf.edu.vn>
2023-12-27 10:10:21 +0700

    lọc theo giá tiền và chi tiết sản phẩm

---
+++ src/main/java/vn/edu/hcmuaf/fit/controllers/web/products/FilterByPriceController.java
@@ -0,0 +1,38 @@
+package vn.edu.hcmuaf.fit.controllers.web.products;
+
+import vn.edu.hcmuaf.fit.dao.ProductDAO;
+import vn.edu.hcmuaf.fit.entity.Products;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "FilterByPriceController", value = "/FilterByPriceController")
+public class FilterByPriceController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String priceRange = request.getParameter("priceRange");
+        if (priceRange == null) {
+            priceRange = "";
+        }
+        try{
+            Integer.parseInt(priceRange);
+        }catch (NumberFormatException e){
+            priceRange = "";
+        }
+        ProductDAO dao = new ProductDAO();
+        List<Products> listProducts = dao.getListProductsByPriceRange(priceRange);
+
+        System.out.println(listProducts.size());
+        request.setAttribute("endP",1);
+        request.setAttribute("listProduct", listProducts);
+        request.getRequestDispatcher("cuahang.jsp").forward(request, response);
+    }
+}
---
+++ src/main/java/vn/edu/hcmuaf/fit/controllers/web/products/ProductDetailController.java
@@ -0,0 +1,33 @@
+package vn.edu.hcmuaf.fit.controllers.web.products;
+
+import vn.edu.hcmuaf.fit.dao.ProductDAO;
+import vn.edu.hcmuaf.fit.entity.Products;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ProductDetailController", value = "/ProductDetailController")
+public class ProductDetailController extends HttpServlet {
+    public ProductDAO productDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        productDAO = new ProductDAO();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productId = request.getParameter("id_Product");
+        Products product = productDAO.getProductsDetail(productId);
+        System.out.println(product.toString());
+        request.setAttribute("product",product);
+        request.getRequestDispatcher("chitietsanpham.jsp").forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
